7e18015aef224e0c1084a257699c315028c374f8
Naotsun <51815450+Naotsun19B@users.noreply.github.com>
2019-10-25 00:14:37 +0900

    Maked PDF viewer for editor

---
+++ Plugins/PDFImporter/Source/PDFImporterEd/PDFImporterEd.Build.cs
@@ -0,0 +1,36 @@
+// Some copyright should be here...
+
+using UnrealBuildTool;
+
+public class PDFImporterEd : ModuleRules
+{
+	public PDFImporterEd(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+                "CoreUObject",
+                "Engine",
+                "InputCore",
+                "SlateCore",
+                "PDFImporter",
+				// ... add other public dependencies that you statically link with here ...
+			}
+			);
+			
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...	
+			}
+			);
+	}
+}
